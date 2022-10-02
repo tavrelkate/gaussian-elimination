ace764f9782a0df1e5a6982baebe4103a50a88da
tavrelkate <tavrelkate@gmail.com>
2022-10-02 17:08:19 +0300

    some descriptions

--- lib/gaussian_elimination.rb
+++ lib/gaussian_elimination.rb
@@ -25,6 +25,7 @@ module Matrix
     def each_array_element
       array.each_with_index do |_column, column_index|
         array.each_with_index do |_row, row_index|
+          # We proccess under diagonal and diagonal elements only
           yield(column_index, row_index) unless row_index < column_index
         end
       end
@@ -34,18 +35,23 @@ module Matrix
       row_index == column_index
     end
 
+    # Transforming diagonal elements to 1. Then updating the rest elements of the row 
+    # Multiplying them to 1.0 / diagonal element
     def focus_element_to_eq_one_and_update_row
       current_row.map! { |el| el * 1.0 / focus_element }
 
       focus_element_to_be_positive_and_update_row if current_element.negative?
     end
 
+    # Transforming under diagonal elements to 0. Then updating the rest elements of the row 
+    # Reduce them to diagonal element * current under diagonal element
     def focus_element_to_eq_zero_and_update_row
       current_row.each_with_index do |_element, index|
         current_row[index] -= current_row_transposed[index] * focus_element
       end
     end
 
+    # Transforming element to possitive. Then updating the rest elements of the row 
     def focus_element_to_be_positive_and_update_row
       current_row.map!(&:-@)
     end
